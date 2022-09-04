ef2e9507eeb9def42b9d0faf975b223307d31f6a
pujiyanto <puji.yantov@gmail.com>
2022-09-04 20:13:16 +0700

    update zoom level dan routing

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ getData();
 
 async function getData() {
     // const url = "http://127.0.0.1:7000/api/collections"
-    // const url = "https://dm.fujiyantov.id/api/collections"
-    const url = "https://cms.borobudurside.com/api/collections"
+    const url = "https://dm.fujiyantov.id/api/collections"
+    // const url = "https://cms.borobudurside.com/api/collections"
     const response = await fetch(url)
     if (response) {
 
